59987e540f93478d1f910fd4ab255206c547eca3
Saulius Stasiukaitis <stasiukaitis.saulius@gmail.com>
2016-10-31 09:28:29 +0100

    Update license from Shop to Module

--- core/invoicepdfpdf.php
+++ core/invoicepdfpdf.php
@@ -1,23 +1,25 @@
 <?php
 /**
- * This file is part of OXID eShop Community Edition.
+ * This file is part of OXID eSales Invoice PDF module.
  *
- * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * OXID eSales Invoice PDF module is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * OXID eSales Invoice PDF module is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ * along with OXID eSales Invoice PDF module.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @link      http://www.oxid-esales.com
+ * @category      module
+ * @package       oeinvoicepdf
+ * @author        OXID eSales AG
+ * @link          http://www.oxid-esales.com
  * @copyright (C) OXID eSales AG 2003-2016
- * @version   OXID eShop CE
  */
 
 $myConfig = oxRegistry::getConfig();
