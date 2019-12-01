17ed4b8ee13b85c8e9467de706868ca14e724af2
inserv@winde-ganzig.de <mnisw1966gh>
2019-12-01 19:04:21 +0100

    required field for review

--- models/invoicepdfoxorder.php
+++ models/invoicepdfoxorder.php
@@ -296,9 +296,9 @@ class InvoicepdfOxOrder extends InvoicepdfOxOrder_parent
     protected function _setDeliveryAddressToPdf($oPdf)
     {
         $oLang = \OxidEsales\Eshop\Core\Registry::getLang();
-        $sSal = $this->oxorder__oxdelsal->value;
+        $sSal = $this->oxorder__oxdelname->value;
         try {
-            $sSal = $oLang->translateString($this->oxorder__oxdelsal->value, $this->getSelectedLang());
+            $sSal = $oLang->translateString($this->oxorder__oxdelname->value, $this->getSelectedLang());
         } catch (Exception $e) {
         }
         $oPdfBlock = oxNew('InvoicepdfBlock');
